0b15a0c3ff068e6dd9cea05225473a432116a555
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2023-06-10 02:35:01 -0400

    Remove server

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
+import { Route, Routes } from 'react-router-dom';
+
+import Navbar from './components/navbar';
+import RecordList from './components/recordList';
+import Edit from './components/edit';
+import Create from './components/create';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<RecordList />} />
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
     </div>
   );
 }
-
 export default App;
